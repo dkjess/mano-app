8999bbb76dc897c3df1a0f9c748f648ccadf37c9
dkjess <dkjess@gmail.com>
2025-08-31 18:02:49 +0200

    Initial Commit

---
+++ Mano/ManoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManoApp.swift
+//  Mano
+//
+//  Created by Jess Wambui Olsen on 31/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ManoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
